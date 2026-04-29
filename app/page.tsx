057baabc600c4782e18bbcfe2459d9a84810983f
profdrtemelyilmazmedya-create <profdrtemelyilmazmedya@gmail.com>
2026-04-29 13:41:00 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 export default function Home() {
   return (
-    <main style={{ margin: 0 }}>
+    <main
+      style={{
+        margin: 0,
+        minHeight: "100vh",
+        backgroundColor: "#f6f3ef",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <img
         src="/hero.jpg"
         alt="Areteus Sağlık"
         style={{
           width: "100%",
-          height: "100vh",
-          objectFit: "cover",
+          height: "auto",
+          maxHeight: "100vh",
+          objectFit: "contain",
+          display: "block",
         }}
       />
     </main>
