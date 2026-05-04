5ce5f301e691061b6efdb3992f9884ed207dd533
profdrtemelyilmazmedya-create <profdrtemelyilmazmedya@gmail.com>
2026-05-04 11:23:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://tqgommhknafkikshzqgd.supabase.co",
-  "BURAYA_SB_KEY"
+  "sb_publishable_Ui5QUNbwt-e3frr1VUxjew_NA6bbBXF"
 );
 
 const countries = [
